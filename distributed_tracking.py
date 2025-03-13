e25beffb85a92d9fb244fdb2517bc736be264b4d
hyperfraise <damien.menigaux@polytechnique.edu>
2025-03-13 16:04:18 +0100

    Add tensorrt inference

--- distributed_tracking.py
+++ distributed_tracking.py
@@ -10,6 +10,14 @@ import torchvision.transforms as T
 from ultralytics.trackers.byte_tracker import BYTETracker
 from types import SimpleNamespace
 from src.core import YAMLConfig  # Your custom config loader
+import time
+import contextlib
+import collections
+from collections import OrderedDict
+import tensorrt as trt
+import pickle
+import boto3
+from io import BytesIO
 
 # Paths
 
@@ -29,6 +37,127 @@ CLASS_NAMES = {
     11: "Travelling Bag",
     12: "Shop's Trolley",
 }
+"""
+Copyright (c) 2024 The D-FINE Authors. All Rights Reserved.
+"""
+
+
+class TimeProfiler(contextlib.ContextDecorator):
+    def __init__(self):
+        self.total = 0
+
+    def __enter__(self):
+        self.start = self.time()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.total += self.time() - self.start
+
+    def reset(self):
+        self.total = 0
+
+    def time(self):
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        return time.time()
+
+
+class TRTInference(object):
+    def __init__(
+        self,
+        engine_path,
+        device="cuda:0",
+        backend="torch",
+        max_batch_size=1,
+        verbose=False,
+    ):
+        self.engine_path = engine_path
+        self.device = device
+        self.backend = backend
+        self.max_batch_size = max_batch_size
+
+        self.logger = (
+            trt.Logger(trt.Logger.VERBOSE) if verbose else trt.Logger(trt.Logger.INFO)
+        )
+
+        self.engine = self.load_engine(engine_path)
+        self.context = self.engine.create_execution_context()
+        self.bindings = self.get_bindings(
+            self.engine, self.context, self.max_batch_size, self.device
+        )
+        self.bindings_addr = OrderedDict((n, v.ptr) for n, v in self.bindings.items())
+        self.input_names = self.get_input_names()
+        self.output_names = self.get_output_names()
+        self.time_profile = TimeProfiler()
+
+    def load_engine(self, path):
+        trt.init_libnvinfer_plugins(self.logger, "")
+        with open(path, "rb") as f, trt.Runtime(self.logger) as runtime:
+            return runtime.deserialize_cuda_engine(f.read())
+
+    def get_input_names(self):
+        names = []
+        for _, name in enumerate(self.engine):
+            if self.engine.get_tensor_mode(name) == trt.TensorIOMode.INPUT:
+                names.append(name)
+        return names
+
+    def get_output_names(self):
+        names = []
+        for _, name in enumerate(self.engine):
+            if self.engine.get_tensor_mode(name) == trt.TensorIOMode.OUTPUT:
+                names.append(name)
+        return names
+
+    def get_bindings(
+        self, engine, context, max_batch_size=32, device=None
+    ) -> OrderedDict:
+        Binding = collections.namedtuple(
+            "Binding", ("name", "dtype", "shape", "data", "ptr")
+        )
+        bindings = OrderedDict()
+
+        for i, name in enumerate(engine):
+            shape = engine.get_tensor_shape(name)
+            dtype = trt.nptype(engine.get_tensor_dtype(name))
+
+            if shape[0] == -1:
+                shape[0] = max_batch_size
+                if engine.get_tensor_mode(name) == trt.TensorIOMode.INPUT:
+                    context.set_input_shape(name, shape)
+
+            data = torch.from_numpy(np.empty(shape, dtype=dtype)).to(device)
+            bindings[name] = Binding(name, dtype, shape, data, data.data_ptr())
+
+        return bindings
+
+    def run_torch(self, blob):
+        for n in self.input_names:
+            if blob[n].dtype is not self.bindings[n].data.dtype:
+                blob[n] = blob[n].to(dtype=self.bindings[n].data.dtype)
+            if self.bindings[n].shape != blob[n].shape:
+                self.context.set_input_shape(n, blob[n].shape)
+                self.bindings[n] = self.bindings[n]._replace(shape=blob[n].shape)
+
+            assert self.bindings[n].data.dtype == blob[n].dtype, (
+                "{} dtype mismatch".format(n)
+            )
+
+        self.bindings_addr.update({n: blob[n].data_ptr() for n in self.input_names})
+        self.context.execute_v2(list(self.bindings_addr.values()))
+        outputs = {n: self.bindings[n].data for n in self.output_names}
+
+        return outputs
+
+    def __call__(self, blob):
+        if self.backend == "torch":
+            return self.run_torch(blob)
+        else:
+            raise NotImplementedError("Only 'torch' backend is implemented.")
+
+    def synchronize(self):
+        if self.backend == "torch" and torch.cuda.is_available():
+            torch.cuda.synchronize()
 
 
 class Model(nn.Module):
@@ -45,7 +174,7 @@ class Model(nn.Module):
         return outputs
 
 
-def load_custom_model(config_path, checkpoint_path, device="cpu"):
+def load_pytorch_model(config_path, checkpoint_path, device="cpu"):
     """Load model EXACTLY like the working script"""
     cfg = YAMLConfig(config_path, resume=checkpoint_path)
 
@@ -83,7 +212,25 @@ class DetectionResults:
         return xywh
 
 
-def process_video(model, video_path, device="cuda:0", conf_thresh=0.6):
+def process_image(trt_mode, frame, model, transforms, device):
+    # Convert frame to PIL image
+    frame_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    w, h = frame_pil.size
+    orig_size = torch.tensor([[w, h]], dtype=torch.float32, device=device)
+    img_tensor = transforms(frame_pil).unsqueeze(0).to(device)
+    if trt_mode:
+        blob = {
+            "images": img_tensor,
+            "orig_target_sizes": orig_size,
+        }
+        output = model(blob)
+        return output["labels"], output["boxes"], output["scores"]
+    else:
+        with torch.no_grad():
+            return model(img_tensor, orig_size)
+
+
+def process_video(model, video_path, device="cuda:0", conf_thresh=0.6, trt_mode=True):
     """Process a single video and save tracking data as JSON"""
     cap = cv2.VideoCapture(video_path)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
@@ -110,17 +257,9 @@ def process_video(model, video_path, device="cuda:0", conf_thresh=0.6):
         if not ret:
             break
 
-        frame_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        w, h = frame_pil.size
-
-        # Ensure inputs are on the correct device
-        orig_size = torch.tensor([[w, h]], dtype=torch.float32, device=device)
-        img_tensor = transforms(frame_pil).unsqueeze(0).to(device)
-
-        # Run inference
-        with torch.no_grad():
-            labels, boxes, scores = model(img_tensor, orig_size)
-
+        labels, boxes, scores = process_image(
+            trt_mode, frame, model, transforms, device
+        )
         detections = []
         for i in range(len(labels[0])):
             if scores[0][i].item() >= conf_thresh:
@@ -158,14 +297,6 @@ def process_video(model, video_path, device="cuda:0", conf_thresh=0.6):
     return tracking_data
 
 
-import pickle
-import boto3
-import argparse
-import os
-import pickle
-from io import BytesIO
-
-
 def list_s3_videos(s3_client, bucket, prefix):
     """List all video files in the given S3 bucket and prefix."""
     response = s3_client.list_objects_v2(Bucket=bucket, Prefix=prefix)
@@ -194,7 +325,15 @@ def upload_results_to_s3(s3_client, bucket, output_key, data):
 
 
 def process_videos(
-    s3_client, model, bucket, s3_output_bucket, output_prefix, rank, world_size, device
+    s3_client,
+    model,
+    bucket,
+    s3_output_bucket,
+    output_prefix,
+    rank,
+    world_size,
+    device,
+    trt_mode,
 ):
     """Process a subset of videos assigned to this worker."""
     with open("val_2025-02-01_to_2025-02-28_100.json", "r") as f:
@@ -229,7 +368,7 @@ def process_videos(
         result = download_video_from_s3(s3_client, bucket, video_s3_path, local_path)
         if not result:
             continue
-        tracking_data = process_video(model, local_path, device)
+        tracking_data = process_video(model, local_path, device, trt_mode=trt_mode)
 
         # Upload results
         upload_results_to_s3(s3_client, s3_output_bucket, output_key, tracking_data)
@@ -242,10 +381,21 @@ if __name__ == "__main__":
         description="Custom Object Tracker (Distributed Execution)"
     )
     parser.add_argument(
-        "-c", "--config", type=str, required=True, help="Path to YAML config"
+        "-c",
+        "--config",
+        type=str,
+        default="configs/dfine/custom/objects365/dfine_hgnetv2_x_obj2custom.yml",
+        help="Path to YAML config",
     )
     parser.add_argument(
-        "-r", "--resume", type=str, required=True, help="Path to model checkpoint"
+        "-r",
+        "--resume",
+        type=str,
+        default="dfine_bag_detector.pth",
+        help="Path to model checkpoint",
+    )
+    parser.add_argument(
+        "-trt", "--tensorrt", action="store_true", help="Tensorrt inference"
     )
     parser.add_argument(
         "--device", type=str, default="cuda:0", help="Device (cpu/cuda)"
@@ -275,15 +425,20 @@ if __name__ == "__main__":
     s3_client = boto3.client("s3")
 
     # Load model
-    custom_model = load_custom_model(args.config, args.resume, args.device)
+    if args.tensorrt:
+        model = TRTInference("dfine_bag_detector.trt")
+    else:
+        model = load_pytorch_model(args.config, args.resume, args.device)
+
     process_videos(
         s3_client,
-        custom_model,
+        model,
         args.s3_bucket,
         args.s3_output_bucket,
         args.s3_output_prefix,
         args.rank,
         args.world_size,
         args.device,
+        args.tensorrt,
     )
     print("All videos processed.")
